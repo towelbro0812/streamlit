9a0d7c3a6ab790a0c9a9b75d108dec2e2b828601
towelbro <kevinjoy7271571@gmail.com>
2023-03-31 09:56:16 +0800

    更改day17

--- day17/day17.py
+++ day17/day17.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title('st.secrets')
-
+st.title('機密檔案 st.secrets')
+st.write('在跟目錄下新增/.streamlit/secret.toml')
+st.write('資料是以字典形式存在，用字典形式調用，因為是秘密文件，要在gitignore中加入檔案')
+st.code('''st.secrets['message']''')
 st.write(st.secrets['message'])
